8eec8050db59ea24ef02448bbd5d913723465cae
musanliang <864415954@qq.com>
2020-03-20 15:47:37 +0800

    1

--- src/components/content/goods/GoodsList.vue
+++ src/components/content/goods/GoodsList.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="goods">
-    <goods-list-item v-for="item in goods" :goods-item="item"/>
+    <goods-list-item v-for="item in goods" :goods-item="item" :key="/>
   </div>
 </template>
 
 <script>
   import GoodsListItem from './GoodsListItem'
 
-  export default {
+  export default { 
     name: "GoodsList",
     components: {
       GoodsListItem
